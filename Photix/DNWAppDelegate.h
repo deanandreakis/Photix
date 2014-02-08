bf9d7dacb425c96cf6107c1f75e74e5c2f464d2f
Dean Andreakis <deanandreakis@gmail.com>
2014-02-08 13:41:38 -0600

    Initial Commit

---
+++ Photix/DNWAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  DNWAppDelegate.h
+//  Photix
+//
+//  Created by Dean Andreakis on 2/8/14.
+//  Copyright (c) 2014 deanware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
